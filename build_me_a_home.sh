1102cf3cb79c6ff9fcd32ce98a36b1a9418285ff
Angus Kong <ak@appier.com>
2018-12-05 01:38:25 +0000

    Add build_me_a_home.sh for installation

--- build_me_a_home.sh
+++ build_me_a_home.sh
@@ -6,6 +6,11 @@ USER_BIN_INSTALL_PATH=${HOME}/local/bin
 function check_git() {
 }
 
+function get_git_completion_bash() {
+  curl -L wget https://github.com/git/git/raw/master/contrib/completion/git-completion.bash \
+    -o ${HOME}/.git-completion.bash
+}
+
 function fzf_install() {
   git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
   ~/.fzf/install
@@ -14,4 +19,7 @@ function fzf_install() {
 function get_diff_so_fancy() {
   curl -L https://github.com/so-fancy/diff-so-fancy/raw/master/diff-so-fancy \
     -o ${USER_BIN_INSTALL_PATH}/diff-so-fancy
+}
+
+function get_bazel() {
 }
